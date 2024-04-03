f3717f5dd0ed6673a873389efc3dee651301b496
Julya Biazoto <108187223+enTrapTa06@users.noreply.github.com>
2024-04-03 09:48:18 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ const tempoObjetivo4 = new Date("2024-12-13T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -38,23 +39,20 @@ if (tempoFinal > 0){
     } else {
        return [0,0,0,0]
     }
-   }
+}
 
-
-
-function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo (tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo (tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo (tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo (tempos[0])[3];
-
+function atualizaCronometro() {
     for (let i=0; i<contadores.length;i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById("dias"+i).textContent = calculaTempo (tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo (tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo (tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo (tempos[i])[3];
     }
 }
-    
-function comecaCronometro(){
-        atualizaCronometro();
-        setInterval(atualizaCronometro,1000)
-    }
+   
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
 comecaCronometro();
